66905e829e38065f2a7cf0142282cef931e5db61
Ben Scofield <git@turrean.com>
2023-02-25 14:08:44 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+fetch('./data.json').then((response) => response.json()).then(metaphors => {
+  const index = Math.floor(Math.random() * metaphors.length)
+  let current = metaphors[index]
+
+  const hash = window.location.hash
+  if (hash !== "") {
+    const name = hash.slice(1)
+    const candidate = metaphors.find(element => element.name.toLowerCase() === name)
+    if (candidate) {
+      current = candidate
+    }
+  }
+
+  document.querySelector("#metaphor h2").innerText = current.name
+  if (current.description) {
+    document.querySelector("#metaphor p#description").innerText = current.description
+  }
+
+  const touchstones = document.querySelector("#metaphor ul#touchstones");
+  touchstones.innerHTML = "";
+  if (current.touchstones) {
+    for (const item of current.touchstones.sort()) {
+      touchstones.innerHTML += "<li>"+item+"</li>"
+    }
+  }
+
+})
