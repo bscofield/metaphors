5532def4775faf5cf6a0c6b62ab75ce43e328153
Ben Scofield <git@turrean.com>
2023-02-25 20:01:29 -0500

    Recycle icon to refresh the metaphor without a page load

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
-fetch('./data.json').then((response) => response.json()).then(metaphors => {
-  const index = Math.floor(Math.random() * metaphors.length)
-  let current = metaphors[index]
+let metaphors = []
 
+const hydrate = () => {
+  const metaphor = getMetaphor()
+  document.querySelector("#metaphor h2").innerText = metaphor.name
+  document.querySelector("#metaphor a").href = "./#"+metaphor.name.toLowerCase()
+  if (metaphor.description) {
+    document.querySelector("#metaphor p#description").innerText = metaphor.description
+  }
+
+  const touchstones = document.querySelector("#metaphor ul#touchstones");
+  touchstones.innerHTML = "";
+  if (metaphor.touchstones) {
+    for (const item of metaphor.touchstones.sort()) {
+      touchstones.innerHTML += "<li>"+item+"</li>"
+    }
+  }
+}
+
+const getMetaphor = () => {
   const hash = window.location.hash
   if (hash !== "") {
     const name = decodeURI(hash.slice(1))
     const candidate = metaphors.find(element => element.name.toLowerCase() === name)
     if (candidate) {
-      current = candidate
+      return candidate
     }
   }
 
-  document.querySelector("#metaphor h2").innerText = current.name
-  document.querySelector("#metaphor a").href = "./#"+current.name.toLowerCase()
-  if (current.description) {
-    document.querySelector("#metaphor p#description").innerText = current.description
-  }
+  const index = Math.floor(Math.random() * metaphors.length)
+  return metaphors[index]
+}
 
-  const touchstones = document.querySelector("#metaphor ul#touchstones");
-  touchstones.innerHTML = "";
-  if (current.touchstones) {
-    for (const item of current.touchstones.sort()) {
-      touchstones.innerHTML += "<li>"+item+"</li>"
-    }
-  }
-
+fetch('./data.json').then((response) => response.json()).then(data => {
+  metaphors = data
+  hydrate()
 })
